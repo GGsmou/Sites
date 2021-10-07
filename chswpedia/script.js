6f9f5d87541fdbe4d98f78ffd7799825d88b5ea6
GGsmou <svyatoslav.golovin.2005@gmail.com>
2021-10-07 22:40:30 +0300

    test

--- chswpedia/script.js
+++
@@ -1,9 +0,0 @@
-function randomBG(){
-	function getRandomInt(max)
-	{
-		bg = Math.floor(Math.random() * max+1);
-	}
-	getRandomInt(5);
-	$("body").css("background-image",'url(img/bg'+bg+'.jpg)');
-	console.log('url(img/bg'+bg+'.jpg);');
-}
