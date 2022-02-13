b0fa167175a36754d51edd8e01882c23275c4114
GGsmou <svyatoslav.golovin.2005@gmail.com>
2022-02-13 19:08:26 +0200

    test

--- timerpomidor/script.js
+++ timerpomidor/script.js
@@ -1,5 +1,5 @@
 const countFromDate = [
-new Date("Sep 01, 2021 08:30:00").getTime(),
+new Date("Sep 01, 2021 08:00:00").getTime(),
 new Date("Dec 24, 2021 19:10:00").getTime(),
 new Date("Dec 25, 2021 23:44:00").getTime(),
 new Date("Jan 10, 2022 10:25:00").getTime(),
@@ -18,15 +18,15 @@ var x = setInterval(function() {
     var minutesFrom = Math.floor((distanceFrom % (1000 * 60 * 60)) / (1000 * 60));
     var secondsFrom = Math.floor((distanceFrom % (1000 * 60)) / 1000);
 
-    document.getElementById(`ot-t${i+1}`).innerHTML = daysFrom + "d " + hoursFrom + "h " + minutesFrom + "m " + secondsFrom + "s ";
+    document.getElementById(`ot-t${i+1}`).innerHTML = daysFrom + "д " + hoursFrom + "ч " + minutesFrom + "м " + secondsFrom + "с ";
     
     //
-
+    /*
     var distance = -1;
     var d=1;
     while (distance<0) {
       var countDownDate = [
-      new Date(`Sep 01, 202${d} 08:30:00`).getTime(),
+      new Date(`Sep 01, 202${d} 08:00:00`).getTime(),
       new Date(`Dec 24, 202${d} 19:10:00`).getTime(),
       new Date(`Dec 25, 202${d} 23:44:00`).getTime(),
       new Date(`Jan 10, 202${d} 10:25:00`).getTime(),
@@ -40,6 +40,6 @@ var x = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById(`do-t${i+1}`).innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById(`do-t${i+1}`).innerHTML = days + "д " + hours + "ч " + minutes + "м " + seconds + "с "; */
   }
 }, 1000);
