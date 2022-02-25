cfd4ee52e9f2e0909571ce23258f6aa6ded1ef21
GGsmou <svyatoslav.golovin.2005@gmail.com>
2022-02-25 15:31:09 +0200

    test

--- timer/script.js
+++ timer/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 23, 2022 17:00:00").getTime();
+var countDownDate = new Date("Feb 24, 2022 04:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -8,7 +8,7 @@ var x = setInterval(function() {
   var now = new Date().getTime();
 
   // Find the distance between now an the count down date
-  var distance = countDownDate - now;
+  var distance = now - countDownDate;
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
