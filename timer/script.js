7de584a50c57808f615936837a1aab8aa11498de
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-05-21 02:59:06 +0300

    Change timer

--- timer/script.js
+++ timer/script.js
@@ -1,30 +1,30 @@
-const countDate = new Date("Feb 24, 2022 04:00:00").getTime();
-const countFrom = true;
+const countDate = new Date('Jun 23, 2023 16:00:00').getTime();
+const countFrom = false;
 
 let x = setInterval(function () {
-    let now = new Date().getTime();
+  let now = new Date().getTime();
 
-    let distance = countFrom ? now - countDate : countDate - now;
+  let distance = countFrom ? now - countDate : countDate - now;
 
-    let years = 0;
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let years = 0;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    while (days >= 365) {
-        days -= 365;
-        years++;
-    }
-    let years_text = "";
+  while (days >= 365) {
+    days -= 365;
+    years++;
+  }
+  let years_text = '';
 
-    let text = `${years_text}${days}д ${hours}г ${minutes}хв ${seconds}с`;
-    if (years > 0) text = `${years}р ` + text;
+  let text = `${years_text}${days}д ${hours}г ${minutes}хв ${seconds}с`;
+  if (years > 0) text = `${years}р ` + text;
 
-    document.getElementById("output").innerHTML = text;
+  document.getElementById('output').innerHTML = text;
 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("output").innerHTML = "Час вийшов";
-    }
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById('output').innerHTML = 'Час вийшов';
+  }
 }, 1000);
